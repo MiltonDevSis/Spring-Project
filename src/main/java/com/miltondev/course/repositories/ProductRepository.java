1281fb7658f25a24d54591269f1853e8a7470098
MiltonDevSis <milton.dev.sis@gmail.com>
2020-11-17 13:45:34 -0300

    Add entity product

---
+++ src/main/java/com/miltondev/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.miltondev.course.repositories;
+
+import com.miltondev.course.entities.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+}
