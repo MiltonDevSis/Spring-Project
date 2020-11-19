1b0fa475d504dd381618409ebedce0685dac4a96
MiltonDevSis <milton.dev.sis@gmail.com>
2020-11-19 14:36:27 -0300

    Add class OrderItem and auxiliary class OrderItemPK part2

--- src/main/java/com/miltondev/course/config/TestConfig.java
+++ src/main/java/com/miltondev/course/config/TestConfig.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 
 import com.miltondev.course.entities.Category;
 import com.miltondev.course.entities.Order;
+import com.miltondev.course.entities.OrderItem;
 import com.miltondev.course.entities.Product;
 import com.miltondev.course.entities.User;
 import com.miltondev.course.entities.enums.OrderStatus;
 import com.miltondev.course.repositories.CategoryRepository;
+import com.miltondev.course.repositories.OrderItemRepository;
 import com.miltondev.course.repositories.OrderRepository;
 import com.miltondev.course.repositories.ProductRepository;
 import com.miltondev.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -69,5 +74,11 @@ public class TestConfig implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
